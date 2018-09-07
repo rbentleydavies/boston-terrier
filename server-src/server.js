9812e9786d94388341e24372748afc75d1b3b64b
Ross Bentley Davies <ross.b@mmtdigital.co.uk>
2018-09-07 13:16:12 +0100

    Port 80

--- server-src/server.js
+++ server-src/server.js
@@ -52,5 +52,5 @@ socketServer.on('connection', socket => {
   })
 })
 socketServer.attach(httpServer)
-httpServer.listen(8080)
-console.log('Server running at http://127.0.0.1:8080/')
+httpServer.listen(80)
+console.log('Server running at http://127.0.0.1:80/')
